0b657e9784544eabae5abb1f4f51875a69758d7d
Patryk <patryk.ciwinski@gmail.com>
2023-02-16 18:13:37 +0100

    filtering and lambda func

--- zajecia_4.py
+++ zajecia_4.py
@@ -7,3 +7,13 @@ tools[2]='kierownica'
 del tools[3]
 tools.pop(3)
 print(tools)
+
+num = [1,2,3,4,5]
+letters=['a','b','c','d']
+
+num.extend(letters)
+print(num)
+
+nums = [1,2,3,4,5]
+even = list(filter(lambda x:(x%2==0),nums))
+print(even)
