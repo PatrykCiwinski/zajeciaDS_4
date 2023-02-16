c2088bb870caa66c077518dc8ac3c634b34d61ad
Patryk <patryk.ciwinski@gmail.com>
2023-02-16 17:59:19 +0100

    adding and deleting obj in list

---
+++ zajecia_4.py
@@ -0,0 +1,9 @@
+tools = ['lornetka', 'lupa', 'teleskop', 'sonda']
+
+tools.insert(2,'śrubokręt')
+tools.append('drewno')
+tools+=['gwóźdź']
+tools[2]='kierownica'
+del tools[3]
+tools.pop(3)
+print(tools)
